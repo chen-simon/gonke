20192b90bede2bb14afdd2cd0526a03521b4ec22
Simon Chen <simonchen.sc.2002@gmail.com>
2023-01-17 17:38:14 -0500

    Refactor

--- src/app.js
+++ src/app.js
@@ -18,27 +18,28 @@ client.on('ready', () => {
 // Chat bot
 client.on('messageCreate', async interaction => {
   const channel = interaction.channel;
-  let lastMessages = [];
+  try {
+    const messages = await channel.messages.fetch({ limit: 8 });
 
-  channel.messages.fetch({ limit: 8 }).then(messages => {
-    messages.forEach(message => {
-      lastMessages.push(`${namemap[message.author.username]}: ${message.content}\n`);
-    })
-
+    let lastMessages = messages.map(
+      message => `${namemap[message.author.username]}: ${message.content}\n`
+    );
     const lastMessage = lastMessages[0].toLowerCase();
-    if (Math.random() > 0.02 && lastMessage.search("gonke") == -1) {
-      return;
-    }
 
-    if (interaction.author.bot) {
-      return;
-    }
+    if (Math.random() > 0.02 && lastMessage.search("gonke") == -1) { return; }
+    if (interaction.author.bot) { return; }
 
-    chat(lastMessages.reverse().join("")).then(response => {
+    try {
+      const response = await chat(lastMessages.reverse().join(""));
+      
       console.log("gonke: " + response.data.choices[0].text);
       channel.send(response.data.choices[0].text.toLowerCase());
-    });
-  })
+    } catch {
+      channel.send("I am out of money");
+    }
+  } catch {
+
+  }
 });
 
 client.login(TOKEN);
